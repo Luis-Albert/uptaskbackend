62201c5ebd896f68eff1b70eb19649e8c6698b96
Luis Alberto Huaman <huamanluis808@gmail.com>
2024-09-23 21:22:19 -0500

    validacion de datos y actualizacion de rutas

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,10 +1,32 @@
 import type { Request, Response } from "express"
+import User from "../models/User";
+import { hashPassword } from "../utils/auth";
 
 export class AuthController {
+    static createAccount = async (req: Request, res: Response) => {
+        try {
+            const { password, email } = req.body;
 
-    static createAccount = (req: Request, res: Response) => {
-        console.log(req.body);
+            //prevenir duplicados
+            const userExists = await User.findOne({ email });
+            if (userExists) {
+                const error = new Error('El usuario ya esta registrado');
+                return res.status(409).json({ error: error.message });
+            }
+            const user = new User(req.body)
 
+            //hash password
+            user.password = await hashPassword(password);
+
+            await user.save();
+            res.send('Cuenta creada');
+        } catch (error) {
+            res.status(500).json({ error: 'Hubo un error' })
+        }
     }
 
+    static getAllUsers = async (req: Request, res: Response) => {
+        const users = await User.find();
+        res.json(users);
+    }
 }
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,9 +1,31 @@
 import { Router } from "express";
 import { AuthController } from "../controllers/AuthController";
+import { body } from "express-validator";
+import { handleInputErrors } from "../middleware/validation";
 
 
 const router = Router();
 
-router.post('/create-account', AuthController.createAccount)
+router.post('/create-account',
+    body('name')
+        .notEmpty()
+        .withMessage('El nombre no puede ir vacio'),
+    body('password')
+        .isLength({ min: 8 })
+        .withMessage('El password es muy corto, minimo 8 caracteres'),
+    body('password_confirmation')
+        .custom((value, { req }) => {
+            if (value !== req.body.password) {
+                throw new Error('Los password no son iguales');
+            }
+            return true
+        }),
+    body('email')
+        .isEmail()
+        .withMessage('Email no valido'),
+    handleInputErrors,
+    AuthController.createAccount);
+
+router.get('/users', AuthController.getAllUsers);
 
 export default router;
