a1c93443697cd44c68684f3627cbf7972d8fe087
Luis Alberto Huaman <huamanluis808@gmail.com>
2024-09-24 20:27:32 -0500

    use morgan

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,18 @@ import express from 'express';
 import dotenv from "dotenv";
 import { connectDB } from './config/db';
 import authRoutes from "./routes/authRoutes"
+import morgan from 'morgan';
 
 dotenv.config();
 connectDB();
 
 const app = express();
 
+//Logging
+app.use(morgan('dev'));
+
 //Leer datos de formularios
-app.use(express.json())
+app.use(express.json());
 
 //Routes
 app.use('/api/auth', authRoutes)
