3cad65041399d6ec927c68702ab56778ba466846
Luis Alberto Huaman <huamanluis808@gmail.com>
2024-09-27 07:41:17 -0500

    add generate Token vaidated

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,6 +1,8 @@
 import type { Request, Response } from "express"
 import User from "../models/User";
 import { hashPassword } from "../utils/auth";
+import Token from "../models/Token";
+import { generateToken } from "../utils/token";
 
 export class AuthController {
     static createAccount = async (req: Request, res: Response) => {
@@ -18,7 +20,12 @@ export class AuthController {
             //hash password
             user.password = await hashPassword(password);
 
-            await user.save();
+            //Generar el Token
+            const token = new Token();
+            token.token = generateToken();
+            token.user = user.id;
+
+            await Promise.allSettled([user.save(), token.save()])
             res.send('Cuenta creada');
         } catch (error) {
             res.status(500).json({ error: 'Hubo un error' })
